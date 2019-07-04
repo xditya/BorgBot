9ba185256054f1a7e791b6d5e5231e740f8adf1a
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-04 18:14:36 +0530

    Update emojis.py

--- stdplugins/emojis.py
+++ stdplugins/emojis.py
@@ -14,7 +14,7 @@ async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.5
-    animation_ttl = range(0, 33)
+    animation_ttl = range(0, 34)
     input_str = event.pattern_match.group(1)
     if input_str == "shrug":
         await event.edit("¯\_(ツ)_/¯")
