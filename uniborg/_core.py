79d720c26db69ad7c2573742b2a91a85ed04469e
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-05 23:36:01 +0530

    Update _core.py

--- uniborg/_core.py
+++ uniborg/_core.py
@@ -67,6 +67,28 @@ async def send_plug_in(event):
     await event.delete()
 
 
+@borg.on(util.admin_cmd(pattern="delete (?P<shortname>\w+)$"))  # pylint:disable=E0602
+async def delete_plug_in(event):
+    if event.fwd_from:
+        return
+    message_id = event.message.id
+    input_str = event.pattern_match["shortname"]
+    the_plugin_file = "./stdplugins/{}.py".format(input_str)
+    start = datetime.now()
+    await borg.delete_file(  # pylint:disable=E0602
+        event.chat_id,
+        the_plugin_file,
+        force_document=True,
+        allow_cache=False,
+        reply_to=message_id
+    )
+    end = datetime.now()
+    time_taken_in_ms = (end - start).seconds
+    await event.edit("deleted {} in {} seconds".format(input_str, time_taken_in_ms))
+    await asyncio.sleep(DELETE_TIMEOUT)
+    await event.delete()
+
+
 @borg.on(util.admin_cmd(pattern="install"))  # pylint:disable=E0602
 async def install_plug_in(event):
     if event.fwd_from:
