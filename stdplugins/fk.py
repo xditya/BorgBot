de62c31f04db3686dc001406496db9a426f00bff
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-05 21:12:31 +0530

    Uploaded New Plugin

---
+++ stdplugins/fk.py
@@ -0,0 +1,37 @@
+"""Emoji
+
+Available Commands:
+
+.fk"""
+
+from telethon import events
+
+import asyncio
+
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 3
+    animation_ttl = range(0, 9)
+    input_str = event.pattern_match.group(1)
+    if input_str == "fk":
+        await event.edit(input_str)
+        animation_chars = [
+            "💠",
+            "💠💠\n💠",
+            "💠💠💠\n💠\n💠",
+            "💠💠💠💠\n💠\n💠\n💠💠",
+            "💠💠💠💠💠\n💠\n💠\n💠💠💠",
+            "💠💠💠💠💠\n💠\n💠\n💠💠💠💠",
+            "💠💠💠💠💠\n💠\n💠\n💠💠💠💠\n💠",
+            "💠💠💠💠💠\n💠\n💠\n💠💠💠💠\n💠\n💠",
+            "💠💠💠💠💠\n💠\n💠\n💠💠💠💠\n💠\n💠\n💠"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 9])
