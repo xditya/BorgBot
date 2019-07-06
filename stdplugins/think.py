5ff6a09ae32e59da794b6f273b72b08f7acbf718
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-07 00:16:40 +0530

    Create think.py

---
+++ stdplugins/think.py
@@ -0,0 +1,64 @@
+"""Emoji
+
+Available Commands:
+
+.think"""
+
+from telethon import events
+
+import asyncio
+
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.1
+    animation_ttl = range(0, 288)
+    input_str = event.pattern_match.group(1)
+    if input_str == "think":
+        await event.edit(input_str)
+        animation_chars = [
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING",
+            "THI&K#N₹",
+            "T+IN@I?G",
+            "¿H$NK∆NG",
+            "¶H×NK&N*",
+            "NGITHKIN",
+            "T+I#K@₹G",
+            "THINKING"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 36])
