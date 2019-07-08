df46fcf05bed1cb4eb7367a9451e53a77b98d2a8
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-08 17:43:45 +0530

    Update ok.py

--- stdplugins/ok.py
+++ stdplugins/ok.py
@@ -14,7 +14,7 @@ from uniborg.util import admin_cmd
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.0001
+    animation_interval = 0.00001
     animation_ttl = range(0, 360)
     input_str = event.pattern_match.group(1)
     if input_str == "ok":
@@ -43,4 +43,4 @@ async def _(event):
         for i in animation_ttl:
         	
             await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 17])
+            await event.edit(animation_chars[i % 180])
