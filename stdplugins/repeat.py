c5b67ffd69e7793bff1621d5b8e641af377c5e54
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-07-19 14:47:46 +0530

    Update repeat.py

--- stdplugins/repeat.py
+++ stdplugins/repeat.py
@@ -5,8 +5,8 @@ from uniborg.util import admin_cmd
 
 @borg.on(admin_cmd("repeat ?(.*)"))
 async def _(event):
-    message = int(event.text[10:])
-    count = int(event.text[8:10])
+    count = int(event.text[10:8])
+    message = (event.text[:10])
     repmessage = message * count
-    await wait([event.respond(repmessage)for i in range(message)])
+    await wait([event.respond(repmessage)for i in range(count)])
     await event.delete()
