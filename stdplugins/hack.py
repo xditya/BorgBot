9c29f4c0cf41b0536cabe165987d2d7c90e1c158
ArnabXD <52126165+ArnabXD@users.noreply.github.com>
2019-07-25 13:06:08 +0530

    Update hack.py

--- stdplugins/hack.py
+++ stdplugins/hack.py
@@ -48,7 +48,7 @@ async def _(event):
             "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
             "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
             "`Hacking... 100%\n█████████████████████████ `",
-            "`Targeted Account Successfully Hacked...\n\nPay 999$ To @refundisilleg To Remove This Hack and take your acc back`"
+            "`Targeted Account Successfully Hacked...\n\nPay 69$ To @arnab431 To Remove This Hack and take your acc back`"
         ]
 
         for i in animation_ttl:
