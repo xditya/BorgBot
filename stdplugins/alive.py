6cbd8d9f68481ba3aff7185b589941c5df5cec50
ArnabXD <52126165+ArnabXD@users.noreply.github.com>
2019-07-26 23:09:16 +0530

    Update alive.py

--- stdplugins/alive.py
+++ stdplugins/alive.py
@@ -1,11 +1,23 @@
-"""Command .alive"""
-from telethon import events
+"""Command To Check Bot is Alive or Not
+.alive"""
 import asyncio
-import os
-import sys
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
 
-@borg.on(events.NewMessage(pattern=r"\.alive", outgoing=True))
-async def test(event):
+
+@borg.on(admin_cmd("alive"))
+async def _(event):
     if event.fwd_from:
-        return 
-    await event.edit("I am alive u noob ,\nStop Checking (;_;) again and again")      
+        return
+    mentions = "`Zinda Hu Sar (^^)/\n\nTelethon version: 1.9.0\nPython: 3.7.3\nUser: @arnab431\nDatabase Stats: Databases functioning normally!`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
