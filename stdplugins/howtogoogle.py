a4d230d7f17083ca4f26f0056bcf35ff0853ae36
Arnab Paryali <52126165+ArnabXD@users.noreply.github.com>
2019-07-29 01:56:28 +0530

    Update howtogoogle.py
    Fixed Hyperlink Issue

--- stdplugins/howtogoogle.py
+++ stdplugins/howtogoogle.py
@@ -20,6 +20,6 @@ async def _(event):
     sample_url = "https://da.gd/s?url=https://lmgtfy.com/?q={}%26iie=1".format(input_str.replace(" ","+"))
     response_api = requests.get(sample_url).text
     if response_api:
-        await event.edit("[{}]({})\nThank me Later ".format(input_str,response_api.rstrip()))
+        await event.edit("[{}]({})\n\n`Thank me Later` ".format(input_str,response_api.rstrip()))
     else:
         await event.edit("something is wrong. please try again later.")
