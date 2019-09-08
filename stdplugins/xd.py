b0fbf1dbd718d9853ec2dc4bce0ba12db9460558
Arnab Paryali <52126165+ArnabXD@users.noreply.github.com>
2019-09-08 10:07:39 +0530

    Update and rename muth.py to xd.py

--- stdplugins/xd.py
+++ stdplugins/xd.py
@@ -1,14 +1,11 @@
 """
 Available Commands
+.lul
 .muth
-Plugin Fixed by @Arnab431"""
+Plugin by @Arnab431"""
 
 from telethon import events
-
 import asyncio
-
-
-
 
 
 @borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
@@ -57,3 +54,27 @@ async def _(event):
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 10])
 
+
+@borg.on(events.NewMessage(pattern=r"\.(.*)", outgoing=True))
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+    animation_ttl = range(0, 30)
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "lul":
+        await event.edit(input_str)
+        animation_chars = [
+
+            "L",
+            "LO",
+            "LOL"
+        ]
+
+        for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 3])
