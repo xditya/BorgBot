8b664d9b01835a08104db465ddf8c26fef3cd1f8
xditya <58950863+xditya@users.noreply.github.com>
2019-12-19 20:06:05 +0530

    Update _help.py

--- stdplugins/_help.py
+++ stdplugins/_help.py
@@ -54,4 +54,4 @@ async def _(event):
     result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
     result = result.stringify()
     logger.info(result)  # pylint:disable=E0602
-    await event.edit("""@arnab431""")
+    await event.edit("""@xditya""")
