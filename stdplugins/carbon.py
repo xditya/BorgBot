cb8c3b46033ed12a58c60f1aec9c4cd5a83eb1c3
xditya <58950863+xditya@users.noreply.github.com>
2019-12-24 10:15:14 +0530

    Update carbon.py

--- stdplugins/carbon.py
+++ stdplugins/carbon.py
@@ -63,7 +63,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="《《 CARBON by [BorgBot](github.com/ArnabXD/BorgBot) 》》",
+         caption="《《 CARBON by [BorgBot](github.com/xditya/BorgBot) 》》",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
